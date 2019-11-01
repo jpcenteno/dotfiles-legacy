24a4d1003e5a04ff73de48ea8dace53e21e7d220
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2019-11-01 11:29:43 -0300

    add readline config and installer.

---
+++ readline/install.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# This script sets up my readline(3) config for the current user.
+
+set -eu
+
+. ./log.sh # Load logging lib
+
+log_info "Setting up readline(3) config."
+
+log_debug "Linking inputrc"
+ln -sf "${PWD}/inputrc" "${HOME}/.inputrc"
+
+log_info "Finished setting up readline(3) config."
